b4e69cfc866282a59b1c412e189c7c5a64198f52
zhouweijie <1309920680@qq.com>
2017-10-20 09:14:45 +0800

    update

---
+++ application/common/model/admin/AdminAuthGroupModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\common\model\admin;
+
+use think\Model;
+
+class AdminAuthGroupModel extends Model
+{
+    //
+}
---
+++ application/common/model/admin/AdminModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\common\model\admin;
+
+use think\Model;
+
+class AdminModel extends Model
+{
+    protected $name = "admin";
+}
