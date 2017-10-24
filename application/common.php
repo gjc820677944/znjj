0b415eebad76e8a2ab21169fd826301cc6dc9cb0
zhouweijie <1309920680@qq.com>
2017-10-24 17:33:54 +0800

    update

--- application/common.php
+++ application/common.php
@@ -87,6 +87,71 @@ function getImage($url,$save_dir='',$filename='',$type=0)
     return array('file_name'=>$filename,'save_path'=>$save_dir.$filename,'error'=>0);
 }
 
+/**
+ * 删除二位数组中指定的字段
+ * @param array $arrs 二维数组
+ * @param array $fields 需要删除的字段
+ */
+function array_remove_fields($arrs, $fields){
+    if(is_string($fields)){
+        $fields = explode(',', $fields);
+    }
+    foreach ($arrs as $k=>$v){
+        foreach ($fields as $field)
+        {
+            if(isset($v[$field])){
+                unset($v[$field]);
+            }
+        }
+        $arrs[$k] = $v;
+    }
+    return $arrs;
+}
+
+/**
+ * 发送HTTP请求方法
+ * @param  string $url    请求URL
+ * @param  array  $params 请求参数
+ * @param  string $method 请求方法GET/POST
+ * @return array  $data   响应数据
+ */
+function http($url, $params = [], $method = 'GET', $header = array(), $multi = false){
+    $opts = array(
+        CURLOPT_TIMEOUT        => 30,
+        CURLOPT_RETURNTRANSFER => 1,
+        CURLOPT_SSL_VERIFYPEER => false,
+        CURLOPT_SSL_VERIFYHOST => false,
+        CURLOPT_HTTPHEADER     => $header
+    );
+
+    /* 根据请求类型设置特定参数 */
+    switch(strtoupper($method)){
+        case 'GET':
+            $opts[CURLOPT_URL] = $url . '?' . http_build_query($params);
+            break;
+        case 'POST':
+            //判断是否传输文件
+            $params = $multi ? $params : http_build_query($params);
+            $opts[CURLOPT_URL] = $url;
+            $opts[CURLOPT_POST] = 1;
+            $opts[CURLOPT_POSTFIELDS] = $params;
+            break;
+        default:
+            throw new Exception('不支持的请求方式！');
+    }
+
+    /* 初始化并执行curl请求 */
+    $ch = curl_init();
+    curl_setopt_array($ch, $opts);
+    $data  = curl_exec($ch);
+    $error = curl_error($ch);
+    curl_close($ch);
+    if($error) throw new Exception('请求发生错误：' . $error);
+    return  $data;
+}
+
+
+
 
 
 
