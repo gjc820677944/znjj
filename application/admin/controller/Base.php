df497600bfa2a3bbe3b90ac62800c8cf421743f9
zhouweijie <1309920680@qq.com>
2017-10-25 12:04:23 +0800

    update

--- application/admin/controller/Base.php
+++ application/admin/controller/Base.php
@@ -14,8 +14,9 @@ class Base extends Controller
         $this->request = Request::instance();
         $controller_name = $this->request->controller();
         $action_name = $this->request->action();
+        $http_referer = $this->request->server('HTTP_REFERER');
         $this->assign("controller_name", $controller_name);
         $this->assign("action_name", $action_name);
-        $this->assign("referer_url",  $_SERVER['HTTP_REFERER']);
+        $this->assign("referer_url",  $http_referer);
     }
 }
