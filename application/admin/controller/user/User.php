75497041c48d9143ad37e3975512ab79616d62e2
gjc <820677944@qq.com>
2017-10-25 15:49:56 +0800

    修改

--- application/admin/controller/user/User.php
+++ application/admin/controller/user/User.php
@@ -9,20 +9,55 @@ class User extends Base
 {
     public function index()
     {
-//        $input=$this->request->param();
-////        $keywords = $input['keywords'];
-        $list = Db::name('user')->paginate(10,true,[
-            'type'     => 'bootstrap',
-            'var_page' => 'page',
-        ]);
-        $this->assign('data',$list);
+
+        $input = $this->request->param();
+        $model = new UserModel();
+        if(isset($input['keywords']) && $input['keywords'] !== ''){
+            $keywords = $input['keywords'];
+            $model->where("username like '%$keywords%'")
+                ->whereOr("mobile like '%$keywords%'")
+                ->whereOr("email like '%$keywords%'");
+        }
+        if(isset($input['status']) && $input['status'] !== ''){
+            $status = (int)$input['status'];
+            $model->where("status", $status);
+        }
+        $list = $model->paginate(10);
+
+        $data = [
+            'list' => $list,
+            'page' => $list->render(),
+            'input' => $input,
+            'create_url' => url('create'),
+        ];
+
+        $this->assign('data',$data);
         return $this->fetch();
 
     }
 
-    public function userIinfo()
+    public function create()
     {
+        $data = [
+            'ad_id' => 0,
+            'ad_account' => '',
+            'password' => '',
+            'avatar' => '',
+            'ad_name' => '',
+            'email' => '',
+            'status' => 0,
+        ];
+        $data = [
+            'data' => $data,
+            'post_url' => url('save'),
+        ];
+        return $this->fetch('edit', $data);
+    }
 
+    //添加用户
+    public function addUser()
+    {
+        $input = $this->request->post();
 
     }
 
