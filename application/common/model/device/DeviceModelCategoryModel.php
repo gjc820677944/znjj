e2e9f09e23a0b822a70a17f591c003d9fa1a2793
zhouweijie <1309920680@qq.com>
2017-10-27 16:53:21 +0800

    update

--- application/common/model/device/DeviceModelCategoryModel.php
+++ application/common/model/device/DeviceModelCategoryModel.php
@@ -9,4 +9,13 @@ class DeviceModelCategoryModel extends Model
     protected $name = "device_model_category";
     protected $autoWriteTimestamp = true;
     protected $updateTime = false;
+
+    /**
+     * 获取模型分类列表
+     */
+    public static function lists(){
+        $list = DeviceModelCategoryModel::field("cate_id, cate_name")
+            ->order("sort_by desc, cate_id asc")->select();
+        return $list;
+    }
 }
--- application/common/model/device/DevicePointCategoryModel.php
+++ application/common/model/device/DevicePointCategoryModel.php
@@ -9,4 +9,13 @@ class DevicePointCategoryModel extends Model
     protected $name = "device_point_category";
     protected $autoWriteTimestamp = true;
     protected $updateTime = false;
+
+    /**
+     * 获取功能点分类列表
+     */
+    public static function lists(){
+        $list = DevicePointCategoryModel::field("cate_id, cate_name")
+            ->order("sort_by desc, cate_id asc")->select();
+        return $list;
+    }
 }
