42d6e36dc344c16f55dd83a9af7da6cce543b73b
gjc <820677944@qq.com>
2017-10-31 11:48:00 +0800

    提交

--- application/api/controller/user/User.php
+++ application/api/controller/user/User.php
@@ -224,31 +224,8 @@ class User extends  Father
         $message=$value;
         $addressee="820677944@qq.com";
 
-//        Vendor('PHPMailer.class#phpmailer');
-        include ("../vendor/phpmailer/class.phpmailer.php");
-        $mail = new \PHPMailer();           //实例化PHPMailer对象
-
-        $mail->IsSMTP();                    // 设定使用SMTP服务
-        $mail->CharSet ="UTF-8";    //编码s
-        $mail->SMTPDebug = 0;               // SMTP调试功能 0=关闭 1 = 错误和消息 2 = 消息
-        $mail->SMTPAuth = true;             // 启用 SMTP 验证功能
-        $mail->SMTPSecure = 'ssl';          // 使用安全协议
-        $mail->Host = "smtp.qq.com";      // SMTP 服务器
-        $mail->Port = 465;                  // SMTP服务器的端口号
-        $mail->Username = "820677944@qq.com";    // SMTP服务器用户名
-        $mail->Password = "olumlwjydtpcbbjj";     // SMTP服务器密码
-        $mail->SetFrom('820677944@qq.com', 'yzm');
-        $replyEmail = '';                   //留空则为发件人EMAIL
-        $replyName = '';                    //回复名称（留空则为发件人名称）
-        $mail->AddReplyTo($replyEmail, $replyName);
-        $mail->Subject = $subject;
-        $mail->MsgHTML($message);
-        $mail->AddAddress($addressee, 'jw');
-        $info= $mail->Send();
-
-
-//        Cache::set($email,$value);
-//        sendEmail($email,'验证码',$value);
+        Cache::set($email,$value);
+        sendEmail($email,'验证码',$value);
     }
 
     //绑定邮箱
--- application/common.php
+++ application/common.php
@@ -160,7 +160,7 @@ function http($url, $params = [], $method = 'GET', $header = array(), $multi = f
  */
     function sendEmail($addressee,$subject,$message)
     {
-        Vendor('PHPMailer.class#phpmailer');
+        include ("../vendor/phpmailer/class.phpmailer.php");
         $mail = new \PHPMailer();           //实例化PHPMailer对象
 
         $mail->IsSMTP();                    // 设定使用SMTP服务
@@ -168,11 +168,11 @@ function http($url, $params = [], $method = 'GET', $header = array(), $multi = f
         $mail->SMTPDebug = 0;               // SMTP调试功能 0=关闭 1 = 错误和消息 2 = 消息
         $mail->SMTPAuth = true;             // 启用 SMTP 验证功能
         $mail->SMTPSecure = 'ssl';          // 使用安全协议
-        $mail->Host = "smtp.qq.com"; // SMTP 服务器
+        $mail->Host = "smtp.qq.com";      // SMTP 服务器
         $mail->Port = 465;                  // SMTP服务器的端口号
         $mail->Username = "820677944@qq.com";    // SMTP服务器用户名
         $mail->Password = "olumlwjydtpcbbjj";     // SMTP服务器密码
-        $mail->SetFrom('820677944@qq.com', 'hw');
+        $mail->SetFrom('820677944@qq.com', 'yzm');
         $replyEmail = '';                   //留空则为发件人EMAIL
         $replyName = '';                    //回复名称（留空则为发件人名称）
         $mail->AddReplyTo($replyEmail, $replyName);
