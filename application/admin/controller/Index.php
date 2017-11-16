6fc8ec59f583808fd2b52131a8e1d7e2430c15df
zhouweijie <1309920680@qq.com>
2017-11-16 10:23:53 +0800

    update

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -5,6 +5,6 @@ class Index extends Base
 {
     public function index()
     {
-        return redirect(url("admin.account/index"));
+        return redirect(url("device.deviceModel/index"));
     }
 }
