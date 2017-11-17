b266f17e50b2a2e91c6427d32ea88b67e0e1150c
gjc <820677944@qq.com>
2017-11-17 15:18:19 +0800

    添加设备操作记录

--- application/api/controller/DeviceModel.php
+++ application/api/controller/DeviceModel.php
@@ -2,7 +2,9 @@
 
 namespace app\api\controller;
 
+use app\common\model\device\DeviceLogModel;
 use app\common\model\device\DeviceModelModel;
+use app\common\model\user\UserModel;
 use filehelper\FileHelper;
 
 class DeviceModel extends Base
@@ -24,5 +26,20 @@ class DeviceModel extends Base
         api_return_json(0, $list);
     }
 
+
+    //设备操作记录
+    function logRecord(){
+        $input = $this->requset->param();
+        $input['times']=time();                 //操作时间
+        $input['operate_people']=UserModel::getTokenId();   //操作用户
+
+        $info=DeviceLogModel::create($input);
+        if ($info){
+            echo api_return_json(0,"记录成功" );
+        }else{
+            echo api_return_json(1,"记录失败" );
+        }
+
+    }
 
 }
---
+++ application/common/model/device/DeviceLogModel.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\common\model\device;
+use think\Model;
+
+class DeviceLogModel extends Model
+{
+    protected $name = "device_log";
+
+}
